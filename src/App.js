8f6b51733396b69e05c7a73be6675ac94b3c802f
BELHAJ Manel <77749468+belhajManel@users.noreply.github.com>
2022-07-19 14:14:20 +0100

    added context provider functions

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import React from 'react';
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
+import Cart from './components/Cart/Cart';
+import { useState } from 'react';
+import cardProvider from './store/cardProvider';
+
 function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const showCarteHnadler = () => {
+    setCartIsShown(true);
+  }
+  const hideCarteHnadler = () => {
+    setCartIsShown(false);
+  }
+
   return (
-    <React.Fragment>
-      <Header/>
+    <cardProvider>
+      {cartIsShown && <Cart onClose={hideCarteHnadler} />}
+      <Header onShowCart={showCarteHnadler}/>
       <main>
         <Meals/>
       </main>
-    </React.Fragment>
+    </cardProvider>
   );
 }
 
